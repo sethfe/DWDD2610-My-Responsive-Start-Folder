ede96e35e52a28cbdefd17e423441104afaacb2c
Seth Elliott <sethfe617@gmail.com>
2023-10-01 17:50:39 -0600

    diner assignment completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+function toggleMenu () {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerMenu");
+
+x.onclick = toggleMenu;
