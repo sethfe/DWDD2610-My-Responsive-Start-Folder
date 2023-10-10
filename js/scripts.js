18bff58489cff0439d73f725cc5a7ae1cede15f8
Seth Elliott <sethfe617@gmail.com>
2023-10-09 19:07:22 -0600

    auto dealer assignments

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,49 @@
-// inject current year in footer
-const rightNow = new Date();
-document.querySelector('#year').textContent = rightNow.getFullYear();
+import { reviews} from "../data/reviews.js";
+console.log(reviews);
 
-function toggleMenu () {
+// create empty card
+const myTarget = document.querySelector("#cards");
+
+for (let x = 0; x < reviews.length; x++) {
+
+    // create a section
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+
+    
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+
+    const myRating = document.createElement('div')
+
+
+    for (let step = 0; step<reviews[x].stars; step++) {
+        const reviewIndicator = document.createElement('img')
+        reviewIndicator.src = "../images/crown-solid.svg"
+        myRating.appendChild(reviewIndicator)
+    }
+    
+
+    // build the figure with child elements
+    mySection.appendChild(myName);
+    mySection.appendChild(myRating);
+    mySection.appendChild(myReview);
+
+    // add a new card to a page
+    myTarget.appendChild(mySection);
+} // end for loop
+
+// nav menu starts here
+function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerMenu").classList.toggle("open");
 }
 
-const x = document.getElementById("hamburgerMenu");
-
+const x = document.getElementById('hamburgerMenu');
 x.onclick = toggleMenu;
+
+
+// inject current year in footer
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear(); //end
