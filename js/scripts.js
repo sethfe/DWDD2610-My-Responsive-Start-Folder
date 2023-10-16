fd5b2a8f27633dbabb79de7efe54190d0d4e0006
Seth Elliott <sethfe617@gmail.com>
2023-10-15 18:40:07 -0600

    trying to figure out what is going on

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
-// inject current year in footer
-const rightNow = new Date();
-document.querySelector('#year').textContent = rightNow.getFullYear();
-
-// hamburger menu
 function toggleMenu() {
-    document.getElementById("primaryNavv").classList.toggle("open");
+    document.getElementById("primaryNav").classList.toggle("open");
 }
+
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
