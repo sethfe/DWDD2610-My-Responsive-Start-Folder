dbd5f9240d6521e4b701ac5992fc498a44ed10b0
Seth Elliott <sethfe617@gmail.com>
2023-10-29 19:24:42 -0600

    x mobile assignment

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+
+
+// inject current year in footer
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
