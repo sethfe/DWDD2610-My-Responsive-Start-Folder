350fec22e634a57202190911eb5b95a2c71aca73
Seth Elliott <sethfe617@gmail.com>
2023-11-13 14:52:16 -0700

    tech conference updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
+const hb = document.querySelector('#hamburgerBtn');
+const pn = document.querySelector('#primaryNav');
 
+hb.addEventListener('click', () => {
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+});
 
 
 
