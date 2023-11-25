2d531c34bc91a243ad22be1490dbff86c6fee806
Seth Elliott <sethfe617@gmail.com>
2023-11-24 17:24:41 -0700

    lesson 9 changes all

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-const hb = document.querySelector('#hamburgerBtn');
-const pn = document.querySelector('#primaryNav');
+const hambutton = document.querySelector('#hamburgerBtn');
 
-hb.addEventListener('click', () => {
-    hb.classList.toggle('open');
-    pn.classList.toggle('open');
-});
+hambutton.addEventListener('click', () => {
+    document.querySelector('#primaryNav').classList.toggle('open');
+})
+
 
 
 
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    //start autoplay
+    autoplay: {
+        delay: 5000,
+    },
+  });
