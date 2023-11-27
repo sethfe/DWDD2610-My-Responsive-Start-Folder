395ca8c158032b974952f341f42c3bdf5942e729
Seth Elliott <sethfe617@gmail.com>
2023-11-27 14:01:23 -0700

    hotel-chain assognment

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
-const hambutton = document.querySelector('#hamburgerBtn');
+const hb = document.querySelector('#hamburgerBtn');
+const pw = document.querySelector('#pageWrapper');
+hb.addEventListener('click', () => {
+    pw.classList.toggle('moveOver');
+});
 
-hambutton.addEventListener('click', () => {
-    document.querySelector('#primaryNav').classList.toggle('open');
-})
+//grab the hotel location data
+import { hotels } from '../data/hotels.js';
+//console.log(hotels);
+const hotelCards=document.querySelector('#hotelCards');
+
+hotels.forEach(hotel => {
+    const mySection = document.createElement('section')
+
+    const theImage = document.createElement("img")
+    theImage.src = `images/${hotel.photo}`
+    theImage.alt = hotel.name
+
+    const theName = document.createElement("h2")
+    theName.textContent = hotel.name
+
+    const theAddress = document.createElement("address");
+    theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+
+    const thePhone = document.createElement("a");
+    thePhone.textContent = hotel.phone
+    thePhone.href = `tel:${hotel.phone}`
+
+    mySection.appendChild(theImage)
+    mySection.appendChild(theName)
+    mySection.appendChild(theAddress)
+    mySection.appendChild(thePhone)
+    hotelCards.appendChild(mySection)
+});
 
 
 
@@ -10,20 +39,3 @@ hambutton.addEventListener('click', () => {
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
-
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    effect: 'slide',
-    speed: 2000,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    //start autoplay
-    autoplay: {
-        delay: 5000,
-    },
-  });
