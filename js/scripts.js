bdbe1c6d54ae25651122de88505d755aca959153
Seth Elliott <sethfe617@gmail.com>
2023-12-05 20:04:31 -0700

    portfolio changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,39 @@
-const hb = document.querySelector('#hamburgerBtn');
-const pw = document.querySelector('#pageWrapper');
-hb.addEventListener('click', () => {
-    pw.classList.toggle('moveOver');
+
+
+// get me a list of all the items to watch
+const myListOfItems = document.querySelectorAll('section')
+
+// a comma deliniated list of name/value pairs controlling how the observer works
+let observerOptions = {
+    //null is the default and references the viewport
+    root: null,
+    //alters the viewport. negative values decrease the size.
+    rootMargin: '0px 0px -20px 0px',
+    //0 is barely showing, 1 is fully showing
+    threshold: .25
+  }
+
+// AllItems is a list of all elements being watched
+const myObserver = new IntersectionObserver(allItems => {
+    allItems.forEach(singleItem => {
+        if (singleItem.isIntersecting){
+            hiliteNav(singleItem.target)
+        }
+    })
+}, observerOptions)
+
+// function to hilight the current navigation items
+function hiliteNav(x) {
+	document.querySelector('.active').classList.remove('active');
+	let theid = x.getAttribute('id');
+	let newActiveLink = document.querySelector(`[href="#${theid}"]`)
+	newActiveLink.parentElement.classList.add('active');
+}
+
+//call the function for each element in the list
+myListOfItems.forEach(item => {
+    myObserver.observe(item)
 });
-
-//grab the hotel location data
-import { hotels } from '../data/hotels.js';
-//console.log(hotels);
-const hotelCards=document.querySelector('#hotelCards');
-
-hotels.forEach(hotel => {
-    const mySection = document.createElement('section')
-
-    const theImage = document.createElement("img")
-    theImage.src = `images/${hotel.photo}`
-    theImage.alt = hotel.name
-
-    const theName = document.createElement("h2")
-    theName.textContent = hotel.name
-
-    const theAddress = document.createElement("address");
-    theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
-
-    const thePhone = document.createElement("a");
-    thePhone.textContent = hotel.phone
-    thePhone.href = `tel:${hotel.phone}`
-
-    mySection.appendChild(theImage)
-    mySection.appendChild(theName)
-    mySection.appendChild(theAddress)
-    mySection.appendChild(thePhone)
-    hotelCards.appendChild(mySection)
-});
-
-
-
 
 // inject current year in footer
 const rightNow = new Date();
